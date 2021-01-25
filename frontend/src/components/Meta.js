a806c134ca365e7c88345e0721ef693e6c26a59c
Gaurang Ruparelia <gr2159@nyu.edu>
2021-01-24 22:14:10 -0500

    Corrected the meta tag words

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Homey',
+  description: 'Meet the best homeopaths',
+  keywords: 'homeopathy, online homeopathy, cheap homeopathy, homeopaths',
 }
 
 export default Meta
